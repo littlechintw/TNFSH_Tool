4b14a2a4fd37bee07c4f0122733e11d72f722264
littlechin <littlechintw@gmail.com>
2019-04-22 20:21:05 +0800

    Update tnfsh_5_new_message.py

--- tnfsh_5_new_message.py
+++ tnfsh_5_new_message.py
@@ -5,19 +5,18 @@ import re
 from urllib.parse import unquote as decode
 
 def tnfshnew():
-
     reply = ''
-
     url = 'https://www.tnfsh.tn.edu.tw/files/501-1000-1012-1.php?Lang=zh-tw'
-    resp = requests.get(url)
-    resp.encoding = 'utf8'
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    sesoup = soup.find_all('span', 'ptname ', limit=5)
+    resp = requests.get(url) #取得網頁原始碼
+    resp.encoding = 'utf8' #轉編碼，才不會出現亂碼
+    soup = BeautifulSoup(resp.text, 'html.parser') #利用BeautifulSoup轉換成該套件格式
+    sesoup = soup.find_all('span', 'ptname ', limit=5) #尋找訊息，且限制最多5項資料
 
     for i in range(5):
         reply += str(i+1) + '.'
-        #reply += sesoup[i].find('td', string=re.compile('^2')).string.strip('\n').strip(' ')
-        reply += sesoup[i].find('a').string.strip('\n').strip('\t').strip(' ')
-        reply += sesoup[i].find('a')['href'].strip('\n').strip('\t').strip(' ') + '\n\n'
+        reply += sesoup[i].find('a').string.strip('\n').strip('\t').strip(' ') #取得最新訊息文字內容
+        reply += sesoup[i].find('a')['href'].strip('\n').strip('\t').strip(' ') + '\n\n' #取得最新訊息連結
 
     return reply
+
+print(tnfshnew())
